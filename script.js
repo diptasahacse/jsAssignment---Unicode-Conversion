8b110d94a8cae0356504dc7533d7180931a136c1
diptasahacse <diptasaha.lpu.cse@gmail.com>
2021-12-09 23:34:01 +0600

    add value to char converter

--- script.js
+++ script.js
@@ -2,12 +2,9 @@
 
 function toUnicode(){
     var getStrValue = document.getElementById('charinputarea').value;
-    var arrValue = getStrValue.split('');
     var uniCodes = "";
     var binaryValue = "";
     for (let i = 0; i < getStrValue.length; i++) {
-        // uniCodes=uniCodes+x.charCodeAt(position)+', '
-
         uniCodes= uniCodes+ getStrValue.charCodeAt(i)+", ";
         var currentBinary = to8bitBinary(getStrValue.charCodeAt(i));
         binaryValue = binaryValue+ currentBinary+ ", ";
@@ -23,7 +20,20 @@ function to8bitBinary(number){
     return binary.padStart(8,0);
 }
 
+function toChar(){
+    var getUnicode = document.getElementById('uniinputarea').value;
+    var unicodeArray = getUnicode.split(',');
+    var charValue = "";
+    for (let index = 0; index < unicodeArray.length; index++) {
+        var currentUniValue = Number(unicodeArray[index].trim())
+        var currentUniChar = String.fromCharCode(currentUniValue);
+        charValue = charValue+ currentUniChar+ ", ";
+    }
+    document.getElementById('carvalue').innerText = charValue.slice(0, -2);
 
+    
+}
 
 
 document.getElementById('convertToCode').onclick = toUnicode;
+document.getElementById('convertToChar').onclick = toChar;
