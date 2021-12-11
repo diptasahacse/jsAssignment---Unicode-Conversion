291dcdc50eea0dc1091fe67c45f1a1fa30dffb38
diptasahacse <diptasaha.lpu.cse@gmail.com>
2021-12-12 02:21:52 +0600

    add hex and octal value

--- script.js
+++ script.js
@@ -4,17 +4,33 @@ function toUnicode(){
     var getStrValue = document.getElementById('charinputarea').value;
     var uniCodes = "";
     var binaryValue = "";
+    var hexValue = "";
+    var octalValue = "";
     for (let i = 0; i < getStrValue.length; i++) {
-        uniCodes= uniCodes+ getStrValue.charCodeAt(i)+", ";
-        var currentBinary = to8bitBinary(getStrValue.charCodeAt(i));
-        binaryValue = binaryValue+ currentBinary+ ", ";
         
+        var currentBinary = to8bitBinary(getStrValue.charCodeAt(i));
+        var currentOctal = toOctal(getStrValue.charCodeAt(i));
+        var currentHex = toHex(getStrValue.charCodeAt(i));
+        uniCodes= uniCodes+ getStrValue.charCodeAt(i)+", ";
+        binaryValue = binaryValue+ currentBinary+ ", ";
+        hexValue = hexValue+ currentHex+ ", ";
+        octalValue = octalValue+ currentOctal+ ", ";
         
     }
     document.getElementById('unicodevalue').innerText = uniCodes.slice(0, -2);
     document.getElementById('binaryvalue').innerText = binaryValue.slice(0, -2);
+    document.getElementById('hexvalue').innerText = hexValue.slice(0, -2);
+    document.getElementById('octalvalue').innerText = octalValue.slice(0, -2);
 
 }
+function toOctal(number){
+    return number.toString(8);
+}
+
+function toHex(number){
+    return number.toString(16);
+}
+
 function to8bitBinary(number){
     var binary = number.toString(2);
     return binary.padStart(8,0);
